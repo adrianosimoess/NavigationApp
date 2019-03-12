a4ee2c53856ce7ba18d8eaddc922890913a058e4
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-11 20:41:13 -0400

    commit inicial

---
+++ NavigationApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NavigationApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void vaiPraProximaPage(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new SegPage());
+        }
+    }
+}
---
+++ NavigationApp/SegPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace NavigationApp
+{
+    public partial class SegPage : ContentPage
+    {
+        void voltarProInicio(object sender, System.EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        public SegPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
